57fc5fe25e94fb98904c57184a5123564c90ba31
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-06 16:22:37 +0200

    improve build and start and add index

--- src/Pokemon.ts
+++ src/Pokemon.ts
@@ -1,7 +1,11 @@
-class Pokemon {
+export class Pokemon {
     name: string;
 
     constructor(name: string) {
         this.name = name;
+    }
+
+    printName() {
+        console.log(this.name);
     }
 }
