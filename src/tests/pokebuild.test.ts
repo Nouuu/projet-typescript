4807b1cbb6d5f1486523046b69bd3ef46d560511
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-30 17:32:04 +0200

    create pokebuild tests

---
+++ src/tests/pokebuild.test.ts
@@ -0,0 +1,12 @@
+import {Pokemon} from "../models/Pokemon";
+
+
+let pikachu: Pokemon = new Pokemon({name: 'pikachu', speed: 90, attack: 55, life: 35});
+//Pokemon { name: 'pikachu', speed: 90, attack: 55, life: 35 }
+
+describe('Test pokbild from API', function () {
+    describe('When a valid pokemon name is provided', function () {
+
+    });
+
+})
